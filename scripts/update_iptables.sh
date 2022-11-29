f429d133b1b3f101471026dba9e17ec875fcf7cb
rodfer0x80 <rodfer0x80@proton.me>
2022-11-29 15:26:44 +0000

    .

--- scripts/update_iptables.sh
+++ scripts/update_iptables.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+sudo iptables -I INPUT -p tcp -m tcp --dport 53 -j ACCEPT
