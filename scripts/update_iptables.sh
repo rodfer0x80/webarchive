e42e51d25acad9a481e0b8fa6ff22932dc3143d6
rodfer0x80 <rodfer0x80@proton.me>
2023-08-07 23:34:23 +0100

    [x]

--- scripts/update_iptables.sh
+++ scripts/update_iptables.sh
@@ -4,4 +4,4 @@ sudo iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 5984 -j ACCEPT # couchdb
 sudo iptables -I INPUT -p tcp -m tcp --dport 53 -j ACCEPT
-sudo iptables -I INPUT -p tcp -m tcp --dport all -j DENY
+sudo iptables -I INPUT -p tcp -m tcp --dport * -j DENY
