b3655e128361a73dcdcb6b62cdcb7110414906ad
rodfer0x80 <rodfer0x80@proton.me>
2023-08-11 16:57:53 +0100

    everything just works clean

--- scripts/update_obsidian.sh
+++ scripts/update_obsidian.sh
@@ -4,13 +4,6 @@ test -e ".env" || exit 1
 USERNAME="$(cat .env | head -n 1 | cut -d'=' -f2)"
 PASSWORD="$(cat .env | head -n 2 | tail -n 1 | cut -d'=' -f2)"
 
-if [ "$USERNAME" -eq "" ]; then
-    exit 1
-fi
-if [ "$PASSWORD" -eq "" ]; then
-    exit 1
-fi
-
 LOCAL_INI="/var/www/obsidian/local.ini"
 DATA="/var/www/obsidian/data"
 
