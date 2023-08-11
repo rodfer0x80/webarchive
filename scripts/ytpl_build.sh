2c7610b42a7e9d89c1ad08c6b7fb71f1f6807edd
rodfer0x80 <rodfer0x80@proton.me>
2023-08-11 13:56:43 +0100

    everything just works clean

--- scripts/ytpl_build.sh
+++ scripts/ytpl_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ./ytpl/ytpl && go build main.go && mv ./ytpl ../opt/ && cd ..
+cd ./ytpl/ytpl && go build main.go && mv ./main ../opt/ytpl && cd ..
 sudo docker build -t "ytpl" .
 rm ./opt/ytpl
