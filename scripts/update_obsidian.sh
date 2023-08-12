1381865b0e5f4ef2217489759d6e30b6b67f7770
rodfer0x80 <rodfer0x80@proton.me>
2023-08-12 06:09:52 +0100

    again, ranting about how much js sucks

--- scripts/update_obsidian.sh
+++ scripts/update_obsidian.sh
@@ -4,14 +4,15 @@ LOCAL_INI="/var/www/obsidian/local.ini"
 DATA="/var/www/obsidian/data"
 
 sudo docker ps | head -n 2 | tail -n 1 | cut -d" " -f1 | xargs sudo docker stop
-sudo docker image rm -f couchdb
 
 cd ./obsidian
 test -e $DATA ||\
-   mkdir -p $DATA
+   sudo mkdir -p $DATA
 test -e $LOCAL_INI ||\
-   cp "./local.ini" $LOCAL_INI
-chmod -R 755 $DATA $LOCAL_INI
-sudo docker-compose up -d;
+   sudo cp "./local.ini" $LOCAL_INI
+sudo chmod -R 755 $DATA $LOCAL_INI
+
+sudo docker run --rm -d -t --name obsidian -p "5984:5984" -v "$DATA:/opt/couchdb/data" -v "$LOCAL_INI:/opt/couchdb/etc/local.ini" -e COUCHDB_USER="obsidian" -e COUCHDB_PASSWORD="obsidian" couchdb
+
 cd ..
 
