dd140332276a3dd9083d774b48a4beda75d189a6
rodfer0x80 <rodfer0x80@proton.me>
2023-08-12 05:26:53 +0100

    again, ranting about how much js sucks

--- scripts/ytpl_update.sh
+++ scripts/ytpl_update.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-sudo docker kill $(docker ps -aqf "name=^ytpl$")
+sudo docker stop "$(docker ps -aqf "name=^ytpl$")"
 sudo docker image rm -f ytpl
 cd ./ytpl/ytpl &&\
     go build &&\
     mv ./ytpl ../opt/ytpl &&\
     cd .. &&\
-    sudo docker build -t "ytpl" . &&\
     rm ./opt/ytpl &&\
-   docker-compose up -d
+    docker-compose up -d
