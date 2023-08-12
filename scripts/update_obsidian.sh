e27b4f5c866f662c9310ee89f3d5ef06a05090e1
rodfer0x80 <rodfer0x80@proton.me>
2023-08-12 05:41:50 +0100

    again, ranting about how much js sucks

--- scripts/update_obsidian.sh
+++ scripts/update_obsidian.sh
@@ -3,15 +3,15 @@
 LOCAL_INI="/var/www/obsidian/local.ini"
 DATA="/var/www/obsidian/data"
 
-
-cd ./obsidian &&\
-   test -e $DATA ||\
-   mkdir -p $DATA &&\
-   test -e $LOCAL_INI ||\
-   cp "./local.ini" $LOCAL_INI &&\
-   chmod -R 755 $DATA $LOCAL_INI &&\
-   sudo docker-compose up -d;\
-   cd ..
-
 sudo docker ps | head -n 2 | tail -n 1 | cut -d" " -f1 | xargs sudo docker stop
 sudo docker image rm -f couchdb
+
+cd ./obsidian
+test -e $DATA ||\
+   mkdir -p $DATA
+test -e $LOCAL_INI ||\
+   cp "./local.ini" $LOCAL_INI
+chmod -R 755 $DATA $LOCAL_INI
+sudo docker-compose up -d;
+cd ..
+
