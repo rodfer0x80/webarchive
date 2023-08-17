af52683b1a1ba84e5e892d71f9fdf81148406719
rodfer0x80 <rodfer0x80@proton.me>
2023-08-17 18:29:37 +0100

    my dog wrote this code soz he just ran with the keyboard

--- scripts/update_obsidian.sh
+++ scripts/update_obsidian.sh
@@ -11,16 +11,15 @@ fi
 if [ -z "$PASSWD" ]; then
     exit 2
 fi
-sudo docker ps | head -n 2 | tail -n 1 | cut -d" " -f1 | xargs sudo docker stop
+docker ps | head -n 2 | tail -n 1 | cut -d" " -f1 | xargs sudo docker stop
 
 cd ./obsidian
 test -e $DATA ||\
-   sudo mkdir -p $DATA
+   mkdir -p $DATA
 test -e $LOCAL_INI ||\
-   sudo cp "./local.ini" $LOCAL_INI
-sudo chmod -R 755 $DATA $LOCAL_INI
+   cp "./local.ini" $LOCAL_INI
+chmod -R 770 $DATA $LOCAL_INI
 
-sudo docker run --rm -d -t --name obsidian -p "5984:5984" -v "$DATA:/opt/couchdb/data" -v "$LOCAL_INI:/opt/couchdb/etc/local.ini" -e COUCHDB_USER="$USER" -e COUCHDB_PASSWORD="$PASSWD" couchdb
+docker run --rm -d -t --name obsidian -p "5984:5984" -v "$DATA:/opt/couchdb/data" -v "$LOCAL_INI:/opt/couchdb/etc/local.ini" -e COUCHDB_USER="$USER" -e COUCHDB_PASSWORD="$PASSWD" couchdb
 
-cd ..
 
